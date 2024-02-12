862de67037ca3d63298c4c63bd09e8b7d6e347cc
ehmoG <dkstjddnjs20@gmail.com>
2024-02-12 15:18:33 +0900

    test character Upload

--- unlock_lfs.sh
+++ unlock_lfs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 현재 로그인한 사용자의 이름을 소문자로 변환하여 가져옵니다.
-current_user=$(whoami | tr '[:upper:]' '[:lower:]')
+current_user="ehmoG"
 
 # 현재 디렉토리가 Git 저장소인지 확인
 if ! git rev-parse --git-dir > /dev/null 2>&1; then
@@ -13,7 +13,8 @@ fi
 git lfs locks | tail -n +2 | while read -r file_path lock_owner file_id rest; do
     # lock_owner를 소문자로 변환하여 대소문자를 무시하고 비교
     lock_owner=$(echo "$lock_owner" | tr '[:upper:]' '[:lower:]')
-    if [[ "$lock_owner" == "$current_user" ]]; then
+    current_user_lower=$(echo "$current_user" | tr '[:upper:]' '[:lower:]')   
+    if [[ "$lock_owner" == "$current_user_lower" ]]; then
         echo "Unlocking file: $file_path..."
         git lfs unlock "$file_path"
     fi
